c1c2d095d47980ee92df92e35b5d01fb3ff3797e
Abdelrhman shawky <abdelrhman.shawky156@gmail.com>
2024-03-31 22:19:03 +0200

    Story1
    base class Heeders ,
    Class Diagram

---
+++ BankSystem/Header.h
@@ -0,0 +1,78 @@
+#pragma once
+#include <iostream>
+
+
+class Person {
+
+protected:
+	int id;
+	std::string name;
+	std::string password;
+
+public:
+	virtual void login(int id, std::string password) = 0;
+};
+
+class Client : public Person {
+
+private:
+	double balance;
+
+public:
+
+	void login(int id,std::string password)override{}
+	void deposit(int amount){}
+	void withdraw(int amount){}
+	int chcekBalance(){}
+	void transferMoney(Client resever){}
+
+
+};
+
+class Employee : public Person {
+
+private:
+
+	double salary;
+
+public:
+
+	void login(int id, std::string password)override{}
+
+	void addClient(Client client){}
+
+	Client searchClient(int id){}
+
+	void editClientInfo(std::string name, std::string password){}
+	void editClientInfo(std::string name) {}
+	void editClientInfo(std::string password) {}
+
+	void displayClientInfo(){}
+
+
+
+
+
+};
+
+class Admin : public Employee {
+
+	double salary;
+
+public:
+
+	void login(int id, std::string password)override {}
+
+	void addEmployee(Employee employee){}
+
+	Employee searchEmployee(int id){}
+
+	void editEmployee(int id, std::string password){}
+	void editEmployee(int id) {}
+	void editEmployee(std::string password) {}
+
+
+	void listEmployees(){}
+
+
+};
